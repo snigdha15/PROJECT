e2c2002d58c7f615f4e0dd620973de6bcbf29344
snigdha15 <83350069+snigdha15@users.noreply.github.com>
2021-05-02 13:28:02 +0530

    Update Cash.java

--- src/Cash.java
+++ src/Cash.java
@@ -4,17 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author mukul
- */
 public class Cash extends javax.swing.JFrame {
 
     /**
